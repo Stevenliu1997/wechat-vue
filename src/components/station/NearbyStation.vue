f868fdc7c5f3d998d1a96fad634188b710b2538e
noisyjonah <1119407797@qq.com>
2017-10-13 20:13:29 +0800

    no message

--- src/components/station/NearbyStation.vue
+++ src/components/station/NearbyStation.vue
@@ -1,93 +1,93 @@
 <template>
   <div>
     <mt-cell>
-      <div slot="title">
-        <table width="100%" style="text-align:center;padding:1vh;">
+      <div slot='title'>
+        <table width='100%' style='text-align:center;padding:1vh;'>
           <tr>
-            <td width="5%">
+            <td width='5%'>
             </td>
-            <td width="75%">
-              <el-input placeholder="查找充电站" icon="search" v-model="stationFilter.sitename" :on-icon-click="stationSearch" v-enter @enter.native="stationSearch"></el-input>
+            <td width='75%'>
+              <el-input placeholder='查找充电站' icon='search' v-model='stationFilter.sitename' :on-icon-click='stationSearch' v-enter @enter.native='stationSearch'></el-input>
             </td>
-            <td width="20%">
-              <span @click="changeTab()" style="color:#aaaaaa">{{ tab }}</span>
+            <td width='20%'>
+              <span @click='changeTab()' style='color:#aaaaaa'>{{ tab }}</span>
             </td>
           </tr>
         </table>
-        <el-row :gutter="5" style="padding:1vh;display:flex;justify-content:center;align-items:center;">
-          <el-col :span="7">
-            <el-cascader :options="locationOptions" :show-all-levels="false" :props="locationProps" placeholder="位置区域" size="small" v-model="stationFilter.location"></el-cascader>
+        <el-row :gutter='5' style='padding:1vh;display:flex;justify-content:center;align-items:center;'>
+          <el-col :span='6'>
+            <el-cascader :options='locationOptions' :show-all-levels='false' :props='locationProps' placeholder='位置' size='small' v-model='stationFilter.location' @change='stationSearch'></el-cascader>
           </el-col>
-          <el-col :span="5">
-            <el-select placeholder="距离我" size="small" v-model="stationFilter.distance">
-              <el-option v-for="op in stationFilterList.distanceOptions" :key="op.value" :label="op.label" :value="op.value">
+          <el-col :span='6'>
+            <el-select placeholder='距离' size='small' v-model='stationFilter.distance' @change='stationSearch'>
+              <el-option v-for='op in stationFilterList.distanceOptions' :key='op.value' :label='op.label' :value='op.value'>
               </el-option>
             </el-select>
           </el-col>
-          <el-col :span="5">
-            <el-select placeholder="电站状态" size="small" v-model="stationFilter.state">
-              <el-option v-for="op in stationFilterList.stateOptions" :key="op.value" :label="op.label" :value="op.value">
+          <el-col :span='6'>
+            <el-select placeholder='状态' size='small' v-model='stationFilter.state' @change='stationSearch'>
+              <el-option v-for='op in stationFilterList.stateOptions' :key='op.value' :label='op.label' :value='op.value'>
               </el-option>
             </el-select>
           </el-col>
-          <el-col :span="7">
-            <el-select placeholder="综合排序" size="small" v-model="stationFilter.sort">
-              <el-option v-for="op in stationFilterList.sortOptions" :key="op.value" :label="op.label" :value="op.value">
+          <el-col :span='6'>
+            <el-select placeholder='排序' size='small' v-model='stationFilter.sort' @change='stationSearch'>
+              <el-option v-for='op in stationFilterList.sortOptions' :key='op.value' :label='op.label' :value='op.value'>
               </el-option>
             </el-select>
           </el-col>
         </el-row>
       </div>
     </mt-cell>
-    <mt-tab-container v-model="activeTab">
-      <mt-tab-container-item id="list">
-        <mt-cell v-for="stationInfo in stationInfoList" :key="stationInfo.siteid" @click.native="toStatinInfo(stationInfo.siteid)">
-          <el-row slot="title">
-            <el-col :span="8" style="margin:1vh 0;">
-              <img class="stationIcon" :src="stationIcon" />
+    <mt-tab-container v-model='activeTab'>
+      <mt-tab-container-item id='list'>
+        <mt-cell v-for='stationInfo in stationInfoList' :key='stationInfo.siteid' @click.native='toStationInfo(stationInfo.siteid)' v-infinite-scroll='loadMore' infinite-scroll-disabled='loading' infinite-scroll-distance='10'>
+          <el-row slot='title'>
+            <el-col :span='8' style='margin:1vh 0;'>
+              <img class='stationIcon' :src='stationIcon' />
             </el-col>
-            <el-col :span="16" style="margin:3vh 0;">
-              <el-row style="font-size:1.2em;">
-                <el-col :span="18">
-                  <span style="vertical-align:text-bottom;">{{ stationInfo.sitename }}</span>
+            <el-col :span='16' style='margin:3vh 0;'>
+              <el-row style='font-size:1.2em;'>
+                <el-col :span='18'>
+                  <span style='vertical-align:text-bottom;'>{{ stationInfo.sitename }}</span>
                 </el-col>
-                <el-col :span="6">
-                  <button v-if="stationInfo.hasfast > 0" class="fastButton">快</button>
-                  <button v-if="stationInfo.hasslow > 0" class="slowButton">慢</button>
+                <el-col :span='6'>
+                  <button v-if='stationInfo.hasfast > 0' class='fastButton'>快</button>
+                  <button v-if='stationInfo.hasslow > 0' class='slowButton'>慢</button>
                 </el-col>
               </el-row>
-              <el-row style="margin-top:2vh;font-size:0.8em;">
-                <el-col :span="7" style="margin-top:0.3vh;">
-                  <star-rating :rating="3" :star-size="18" :increment="0.1" :fixed-points="1" :read-only="true" :show-rating="false"></star-rating>
+              <el-row style='margin-top:2vh;font-size:0.8em;'>
+                <el-col :span='10'>
+                  <star-rating :rating='stationInfo.star' :star-size='18' :increment='0.1' :fixed-points='1' :read-only='true' :show-rating='false'></star-rating>
                 </el-col>
-                <el-col :span="10">
-                  <div class="ellipsis">
+                <el-col :span='7'>
+                  <div class='ellipsis'>
                     <span>{{ stationInfo.price }}元/度</span>
                   </div>
                 </el-col>
-                <el-col :span="7">
-                  <span style="color:#666666">距您{{ (stationInfo.distance/1000).toFixed(1) }}km</span>
+                <el-col :span='7'>
+                  <span style='color:#666666'>距您{{ (stationInfo.distance/1000).toFixed(1) }}km</span>
                 </el-col>
               </el-row>
-              <el-row style="margin-top:2vh;font-size:0.6em;">
-                <el-col :span="18">
-                  <div class="ellipsis">
+              <el-row style='margin-top:2vh;font-size:0.6em;'>
+                <el-col :span='18'>
+                  <div class='ellipsis'>
                     <span>{{ stationInfo.position }}</span>
                   </div>
                 </el-col>
-                <el-col :span="6">
-                  <span style="color:#33e02f">空闲{{ stationInfo.pilenum }}</span>
+                <el-col :span='6'>
+                  <span style='color:#33e02f'>空闲{{ stationInfo.pilenum }}</span>
                 </el-col>
               </el-row>
-              <el-row style="margin-top:2vh;">
-                <div class="dashedLine"></div>
+              <el-row style='margin-top:2vh;'>
+                <div class='dashedLine'></div>
               </el-row>
             </el-col>
           </el-row>
         </mt-cell>
       </mt-tab-container-item>
-      <mt-tab-container-item id="map">
-        <navigation :positionList="positionList"></navigation>
+      <mt-tab-container-item id='map'>
+        <navigation :positionList='positionList'></navigation>
       </mt-tab-container-item>
     </mt-tab-container>
   </div>
@@ -135,9 +135,10 @@ export default {
         'pageNumber': 1,
         'pageSize': 10
       },
-      stationInfoList: {},
+      stationInfoList: [],
       stationIcon: stationIcon,
       totalPages: '',
+      loading: false,
       positionList: []
     }
   },
@@ -150,16 +151,19 @@ export default {
         this.stationParams.district = this.stationFilter.location[2]
       }
       if (this.stationFilter.distance !== null || this.stationFilter.distance !== undefined || this.stationFilter.distance !== '') {
-        this.stationParams.distance = this.stationFilter.distance
+        this.stationParams.distance = this.stationFilter.distance * 1000
       }
       if (this.stationFilter.state !== null || this.stationFilter.state !== undefined || this.stationFilter.state !== '') {
         this.stationParams.state = this.stationFilter.state
       }
       if (this.stationFilter.sort !== null || this.stationFilter.sort !== undefined || this.stationFilter.sort !== '') {
+        this.stationParams.multiple = 0
+        this.stationParams.commentfirst = 0
+        this.stationParams.cheapfirst = 0
         if (this.stationFilter.sort === 'multiple') {
           this.stationParams.multiple = 1
         } else if (this.stationFilter.sort === 'distance') {
-          this.stationParams.distance = 1
+          // this.stationParams.distance = 1
         } else if (this.stationFilter.sort === 'commentfirst') {
           this.stationParams.commentfirst = 1
         } else if (this.stationFilter.sort === 'cheapfirst') {
@@ -167,8 +171,12 @@ export default {
         }
       }
       this.$http.post('/siteinformation/find', this.stationParams).then(response => {
-        this.stationInfoList = response.body.pageData
         this.totalPages = response.body.totalPages
+        if (this.stationParams.pageNumber === '1') {
+          this.stationInfoList = response.body.pageData
+        } else {
+          this.stationInfoList = this.stationInfoList.concat(response.body.pageData)
+        }
         for (let stationInfo of this.stationInfoList) {
           this.positionList.push({ lng: stationInfo.longitude, lat: stationInfo.latitude })
         }
@@ -186,6 +194,16 @@ export default {
           id: index
         }
       })
+    },
+    loadMore() {
+      this.loading = true
+      setTimeout(() => {
+        if (this.stationParams.pageNumber < this.totalPages) {
+          this.stationParams.pageNumber++
+          this.stationSearch()
+        }
+        this.loading = false
+      }, 1000)
     }
   },
   created() {
@@ -241,7 +259,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 .filterIcon {
   width: 18px;
